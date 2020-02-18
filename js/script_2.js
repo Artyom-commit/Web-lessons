cc27e8da22950f9b8556205b51c7c99e8a433225
Artyom-commit <longcycle@mail.ru>
2020-02-18 12:52:45 +0300

    очередной

--- js/script_2.js
+++ js/script_2.js
@@ -1,5 +1,16 @@
 var app = new Vue({
     el: "#app_2",
     data: {message: "Hello world",
-           status: false}
+           status: false,
+           inp: '',
+           status_one: false
+        },
+    methods: {
+        fun: function(){
+            console.log("Console message")
+        },
+        console_inp: function(){
+            console.log(this.inp)
+        }
+    }
 })
